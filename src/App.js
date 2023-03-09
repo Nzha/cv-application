c1236738f427511eef396408aa6103ba4929f1e4
Adrien <adrien.biz@gmail.com>
2023-03-09 22:05:25 +0400

    Create basic page layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { PencilSquareIcon } from '@heroicons/react/24/outline';
+
+function ModifyCV() {
+  return (
+    <div className="flex flex-col gap-6">
+      <CVTitle />
+      <ContactInfo />
+    </div>
+  );
+}
+
+function CVTitle() {
+  return (
+    <div className="flex justify-between rounded-2xl bg-white px-16 py-6">
+      <div className="flex">
+        <div className="font-bold">Title</div>
+        <PencilSquareIcon className="text-gray-400 h-5 w-5" />
+      </div>
+      <button>Download</button>
+    </div>
+  );
+}
+
+function ContactInfo() {
+  return <div className="bg-white">Test</div>;
+}
+
+function ViewCV() {
+  return <div className="min-h-full bg-white"></div>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="grid min-h-screen grid-cols-2 gap-12 bg-slate-100 px-36 pt-10">
+      <ModifyCV />
+      <ViewCV />
+    </main>
   );
 }
 
