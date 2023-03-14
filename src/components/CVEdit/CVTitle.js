df57a77ffd341f7b472b0b7ccacb3fd64c7f697d
Adrien <adrien.biz@gmail.com>
2023-03-14 20:19:34 +0400

    Make website more responsive

--- src/components/CVEdit/CVTitle.js
+++ src/components/CVEdit/CVTitle.js
@@ -5,13 +5,13 @@ import {
 
 function CVTitle() {
   return (
-    <div className="sticky flex items-center justify-between bg-white px-9 py-6 shadow-sm lg:static lg:rounded-2xl xl:px-16 -mx-6 lg:mx-0">
+    <div className="sticky -mx-6 flex items-center justify-between bg-white px-9 py-6 shadow-sm lg:static lg:mx-0 lg:rounded-2xl xl:px-16">
       <div className="flex cursor-pointer items-center gap-3 hover:bg-opacity-20 hover:bg-gradient-to-t hover:opacity-70">
         <div className="text-3xl font-bold">My Resume</div>
         <PencilSquareIcon className="h-5 w-5 stroke-2 text-gray-400" />
       </div>
-      <button className="flex  gap-2 rounded-full bg-sky-500 px-5 py-2 text-sm font-semibold leading-5 text-white hover:bg-sky-700">
-        Download
+      <button className="flex  gap-2 rounded-full bg-sky-500 sm:px-5 p-3 sm:py-2 text-sm font-semibold leading-5 text-white hover:bg-sky-700">
+        <div className='hidden sm:block'>Download</div>
         <DocumentArrowDownIcon className="h-5 w-5 stroke-2 text-white" />
       </button>
     </div>
