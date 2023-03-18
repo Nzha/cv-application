dc537757eb3df2171485cc35a1515292a8452c5c
Adrien <adrien.biz@gmail.com>
2023-03-18 21:37:45 +0400

    Clean up modal

--- src/components/CVEdit/CVEdit.js
+++ src/components/CVEdit/CVEdit.js
@@ -3,7 +3,7 @@ import CVTitle from './CVTitle';
 import FullNameForm from './FullNameForm';
 import PersonalInfo from './PersonalInfo';
 import PersonalInfoForm from './PersonalInfoForm';
-// import AddContentModal from './AddContentModal';
+import Modal from './Modal';
 import { PlusIcon } from '@heroicons/react/24/outline';
 
 function CVEdit({ user, setUser, avatar, setAvatar }) {
@@ -56,65 +56,6 @@ function AddContentBtn() {
       </button>
       <Modal showModal={showModal} setShowModal={setShowModal} />
     </div>
-  );
-}
-
-function Modal({ showModal, setShowModal }) {
-  return (
-    showModal && (
-      <>
-        <div className="fixed inset-0 z-50 flex items-center justify-center overflow-y-auto overflow-x-hidden outline-none focus:outline-none">
-          <div className="relative my-6 mx-auto w-auto max-w-3xl">
-            {/*content*/}
-            <div className="relative flex w-full flex-col rounded-lg border-0 bg-white shadow-lg outline-none focus:outline-none">
-              {/*header*/}
-              <div className="flex items-start justify-between rounded-t p-5">
-                <h3 className="text-3xl font-semibold">Add content</h3>
-                <button
-                  className="float-right ml-auto border-0 bg-transparent p-1 text-3xl font-semibold leading-none text-black opacity-5 outline-none focus:outline-none"
-                  onClick={() => setShowModal(false)}
-                >
-                  <span className="block h-6 w-6 bg-transparent text-2xl text-black opacity-5 outline-none focus:outline-none">
-                    ×
-                  </span>
-                </button>
-              </div>
-              {/*body*/}
-              <div className="grid px-6">
-                <div className="cursor-pointer rounded-lg bg-gray-100 px-4 py-5 transition-all hover:scale-[1.02] hover:opacity-90">
-                  <div className="flex gap-2">
-                    <div className="font-semibold">Professional Experience</div>
-                  </div>
-                  <p className="text-gray-500">
-                    A place to highlight your professional experience
-                  </p>
-                </div>
-                <div className="cursor-pointer rounded-lg bg-gray-100 px-4 py-5 transition-all hover:scale-[1.02] hover:opacity-90">
-                  <div className="flex gap-2">
-                    <div className="font-semibold">Education</div>
-                  </div>
-                  <p className="text-gray-500">
-                    Show off your primary education, college, degrees & exchange
-                    semesters
-                  </p>
-                </div>
-              </div>
-              {/*footer*/}
-              <div className="flex items-center justify-end rounded-b">
-                <button
-                  className="background-transparent mr-1 mb-1 px-6 py-2 text-sm font-bold uppercase text-red-500 outline-none transition-all duration-150 ease-linear focus:outline-none"
-                  type="button"
-                  onClick={() => setShowModal(false)}
-                >
-                  Close
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="fixed inset-0 z-40 bg-black opacity-25"></div>
-      </>
-    )
   );
 }
 
---
+++ src/components/CVEdit/Modal.js
@@ -0,0 +1,49 @@
+import { XMarkIcon } from '@heroicons/react/24/outline';
+
+function Modal({ showModal, setShowModal }) {
+  return (
+    showModal && (
+      <>
+        <div className="fixed inset-0 z-50 m-2 flex items-center justify-center overflow-y-auto overflow-x-hidden outline-none focus:outline-none">
+          <div className="relative my-6 mx-auto w-auto max-w-3xl">
+            <div className="relative flex w-full flex-col rounded-2xl border-0 bg-white px-6 py-6 shadow-lg outline-none focus:outline-none sm:px-9">
+              {/*header*/}
+              <div className="flex items-center justify-between pb-6">
+                <h3 className="text-3xl font-semibold">Add content</h3>
+                <button
+                  className="rounded-full bg-gradient-to-r from-sky-500 to-teal-500 p-1 leading-5 text-white hover:from-sky-500 hover:to-sky-500"
+                  onClick={() => setShowModal(false)}
+                >
+                  <XMarkIcon className="h-6 w-6 stroke-2 text-white" />
+                </button>
+              </div>
+              {/*body*/}
+              <div className="grid grid-cols-1 gap-5 sm:grid-cols-2">
+                <div className="cursor-pointer rounded-lg bg-gray-100 px-4 py-5 transition-all hover:scale-[1.02] hover:opacity-90">
+                  <div className="flex gap-2">
+                    <div className="font-semibold">Professional Experience</div>
+                  </div>
+                  <p className="text-gray-500">
+                    A place to highlight your professional experience
+                  </p>
+                </div>
+                <div className="cursor-pointer rounded-lg bg-gray-100 px-4 py-5 transition-all hover:scale-[1.02] hover:opacity-90">
+                  <div className="flex gap-2">
+                    <div className="font-semibold">Education</div>
+                  </div>
+                  <p className="text-gray-500">
+                    Show off your primary education, college, degrees & exchange
+                    semesters
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="fixed inset-0 z-40 bg-black opacity-25"></div>
+      </>
+    )
+  );
+}
+
+export default Modal;
